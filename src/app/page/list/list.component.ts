f669792192fbd5c292a6787cb50bca1235d08c13
Andres Herrera <lherrera>
2022-03-30 19:07:33 -0500

    subscribe empleados en el component list

--- src/app/page/list/list.component.ts
+++ src/app/page/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IEmployee } from 'src/app/interfaces/employee.interface';
 import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
@@ -8,11 +9,19 @@ import { EmployeesService } from 'src/app/services/employees.service';
 })
 export class ListComponent implements OnInit {
 
+  employes: IEmployee[] = [];
+
   constructor(private _serviceEmployees: EmployeesService) { }
 
   ngOnInit(): void {
-    this._serviceEmployees.getEmployees$().subscribe((data) => {
-      console.log(data);
+    this._serviceEmployees.getEmployees$().subscribe({
+      next: (res) => {
+        this.employes = res;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => console.info('getEmployees$ completed')
     })
   }
 
